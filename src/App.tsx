115d4b3df11957eda7b273ff2a065730b6466324
tal <tal.bny3@gmail.com>
2022-11-17 19:55:34 +0200

    handled errors when the text format is not ok

--- src/App.tsx
+++ src/App.tsx
@@ -38,23 +38,33 @@ function App() {
   const [open, setOpen] = React.useState(false);
   const [wordsInputText, setWordsInputText] = useState('');
   const [wordsList, setWordsList] = useState<Word[]>(stub);
+  const [hasError, setHasError] = useState(false);
 
   const changeWordsList = () => {
-    console.log({wordsInputText})
-    const allWords = wordsInputText.split('\n');
-    const finalWordsList: Word[] = [];
-    for (const word of allWords) {
-      const wordParts = word.split(' - ');
-      const spanishTranslation = wordParts[0];
-      const englishTranslation = wordParts[1];
-      finalWordsList.push({
-        english: englishTranslation,
-        spanish: spanishTranslation,
-      })
+    try {
+      console.log({wordsInputText})
+      const allWords = wordsInputText.split('\n');
+      const finalWordsList: Word[] = [];
+      for (const word of allWords) {
+        const wordParts = word.split(' - ');
+        const spanishTranslation = wordParts[0];
+        const englishTranslation = wordParts[1];
+        if (spanishTranslation === undefined || englishTranslation === undefined) {
+          throw new Error('not valid format');
+        }
+        finalWordsList.push({
+          english: englishTranslation,
+          spanish: spanishTranslation,
+        })
+      }
+  
+      setWordsList(finalWordsList);
+      setOpen(false);
+      setHasError(false);
+    } catch(e) {
+      setHasError(true);
     }
 
-    setWordsList(finalWordsList);
-    setOpen(false);
   }
 
   return (
@@ -76,11 +86,15 @@ function App() {
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <div style={{marginBottom: 10}}>List the words to practice</div>
+            <div style={{marginBottom: 5, fontSize: 20}}>List the words to practice</div>
+            <div style={{visibility: hasError ? 'visible' : 'hidden'}} className="errorText">Error: Not valid format</div>
             <div style={{ width: '100%', height: '10rem', paddingRight: 10}} >
             <textarea value={wordsInputText} onChange={(e) => setWordsInputText(e.target.value)} name="Text1" style={{resize: 'none', width: '100%', height: '100%'}}/>
             </div>
-            <div className='applyListButton'><CiPlay1 onClick={changeWordsList} size={30}/></div>
+            <div className='applyListButton'>
+              <CiPlay1 onClick={changeWordsList} size={30}/>
+              
+            </div>
           </Box>
         </Modal>
     </div>
