63629afb93e503c4f15f869697eb0402561156a0
tal <tal.bny3@gmail.com>
2022-11-17 19:11:31 +0200

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,24 @@ import './App.css';
 import Card from './card/card';
 import {AiOutlineUnorderedList} from 'react-icons/ai';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '70%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
 const stub = [{
   english: 'hello', 
   spanish: 'hola'
@@ -16,17 +34,35 @@ const stub = [{
 },]
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
     <div className="App" style={{ padding: 1}}>
         {/* <p>hjel</p> */}
         <div  className='header'>
-          <div style={{backgroundColor: 'white', width: 'fit-content', height: 'fit-content', borderRadius: 10, margin: 20}}>
+          <div onClick={handleOpen} className="listButton">
             <AiOutlineUnorderedList color='rgb(106 106 106)' size={40} />
           </div>
         </div>
         <div className='content' style={{display: 'flex', height: '100%'}}>
           <Card wordsList={stub} />
         </div>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div>List the words to practice</div>
+            <textarea name="Text1" style={{resize: 'none', width: '100%', height: '10rem'}} />
+            
+          </Box>
+        </Modal>
     </div>
   );
 }
