58f218d18286a174202d4f2242b192f5a4a1b51b
tal <tal.bny3@gmail.com>
2022-11-17 18:45:03 +0200

    added button that changes the word randomly

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,17 @@ import './App.css';
 import Card from './card/card';
 import {AiOutlineUnorderedList} from 'react-icons/ai';
 
+const stub = [{
+  english: 'hello', 
+  spanish: 'hola'
+},{
+  english: 'have', 
+  spanish: 'tener'
+},{
+  english: 'como', 
+  spanish: 'how'
+},]
+
 function App() {
   return (
     <div className="App" style={{ padding: 1}}>
@@ -14,7 +25,7 @@ function App() {
           </div>
         </div>
         <div className='content' style={{display: 'flex', height: '100%'}}>
-          <Card word={{english: 'asd', spanish: 'ssqwe'}} />
+          <Card wordsList={stub} />
         </div>
     </div>
   );
--- src/card/card.tsx
+++ src/card/card.tsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import './card.css';
 import {Word} from '../models/word'
+import {AiOutlineArrowLeft} from 'react-icons/ai'
 
 interface Props {
-    word: Word;
+    wordsList: Word[];
 }
 
-function Card({word}: Props) {
+const defaultWord: Word = {english: '...', spanish: '...'};
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+function Card({wordsList}: Props) {
   const [isTranslationHidden, setIsTranslationHidden] = useState(true);
+  const [currentWord, setCurrentWord] = useState(wordsList[0] ?? defaultWord);
+
+  const changeWordRandomly = () => {
+    const randomIndex = getRandomInt(wordsList.length);
+    setCurrentWord(wordsList[randomIndex]);
+  }
 
   return (
     <div className='card'>
-        <p className='originalWord'>{word.english}</p>
-        {!isTranslationHidden && <p className='translationWord'>{word.spanish}</p>}
+        <p className='originalWord'>{currentWord.english}</p>
+        {!isTranslationHidden && <p className='translationWord'>{currentWord.spanish}</p>}
         {isTranslationHidden && <p className='revealButton' onClick={() => setIsTranslationHidden(false)}>reveal</p>}
-        
+        <AiOutlineArrowLeft style={{cursor: 'pointer'}} size={20} onClick={changeWordRandomly} />
     </div>
   );
 }
