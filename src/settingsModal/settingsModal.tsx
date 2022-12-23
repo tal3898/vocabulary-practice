a1952e289631b425d1fa2b150bb20ba54cfced43
tal <tal.bny3@gmail.com>
2022-12-24 00:45:11 +0200

    added option to add words to practice list in the settings popup

--- src/settingsModal/settingsModal.tsx
+++ src/settingsModal/settingsModal.tsx
@@ -8,7 +8,10 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { practiceListSelector } from "../stateManagement/practiceList";
+import {
+  practiceListSelector,
+  setPracticeList,
+} from "../stateManagement/practiceList";
 import {
   practiceAmountSelector,
   setPracticeAmount,
@@ -19,6 +22,10 @@ import {
   isRevealEnabledSelected,
   setIsRevealEnabled,
 } from "../stateManagement/revealOption";
+import { AiOutlinePlus } from "react-icons/ai";
+import { Word } from "../models/word";
+import { isWordInList } from "../utils/wordsList";
+import { learnedListSelector } from "../stateManagement/learnedList";
 
 interface Props {
   isOptionsOpen: boolean;
@@ -29,12 +36,56 @@ export const SettingsModal = ({ isOptionsOpen, setIsOptionsOpen }: Props) => {
   const amountToPractice = useSelector(practiceAmountSelector);
   const isRevealEnabled = useSelector(isRevealEnabledSelected);
   const practiceWords = useSelector(practiceListSelector);
+  const learnedList = useSelector(learnedListSelector);
 
+  const [addedWordsToPractice, setAddedWordsToPractice] = useState<
+    number | undefined
+  >(undefined);
   const [clientAmountToPractice, setClientAmountToPractice] =
     useState(amountToPractice);
   const [isRevealChecked, setIsRevealChecked] = useState(true);
+  const [wordsInputText, setWordsInputText] = useState("");
 
   const dispatch = useDispatch();
+
+  const getWordsListFromInput = () => {
+    const allWords = wordsInputText.split("\n");
+    const finalWordsList: Word[] = [];
+    for (const word of allWords) {
+      const wordParts = word.split(" - ");
+      const spanishTranslation = wordParts[0];
+      const englishTranslation = wordParts[1];
+      if (
+        spanishTranslation !== undefined &&
+        englishTranslation !== undefined
+      ) {
+        finalWordsList.push({
+          english: englishTranslation,
+          spanish: spanishTranslation,
+        });
+      }
+    }
+
+    return finalWordsList;
+  };
+
+  const addCustomWordsToPractice = () => {
+    const clientWordsList: Word[] = getWordsListFromInput();
+    const newPracticedWordsList = [...practiceWords];
+    let addedWordsCount = 0;
+    for (const newWord of clientWordsList) {
+      if (
+        !isWordInList(newWord, newPracticedWordsList) &&
+        !isWordInList(newWord, learnedList)
+      ) {
+        newPracticedWordsList.push(newWord);
+        addedWordsCount++;
+      }
+    }
+
+    setAddedWordsToPractice(addedWordsCount);
+    dispatch(setPracticeList(newPracticedWordsList));
+  };
 
   const saveSettings = () => {
     dispatch(setPracticeAmount(clientAmountToPractice));
@@ -75,6 +126,32 @@ export const SettingsModal = ({ isOptionsOpen, setIsOptionsOpen }: Props) => {
             checked={isRevealChecked}
             onChange={(e) => setIsRevealChecked(e.target.checked)}
           />
+        </div>
+        <div style={{ width: "100%" }}>
+          <div className="wordsToAddTitle">
+            <div>Add words to practice list</div>
+            <div className="addWordsButton" onClick={addCustomWordsToPractice}>
+              <AiOutlinePlus size={20} />
+            </div>
+          </div>
+
+          <div
+            style={{
+              fontSize: 12,
+              visibility:
+                addedWordsToPractice !== undefined ? "visible" : "hidden",
+            }}
+          >
+            Added {addedWordsToPractice} words
+          </div>
+          <div style={{ width: "100%", height: "6rem", paddingRight: 10 }}>
+            <textarea
+              value={wordsInputText}
+              onChange={(e) => setWordsInputText(e.target.value)}
+              name="Text1"
+              style={{ resize: "none", width: "100%", height: "100%" }}
+            />
+          </div>
         </div>
       </DialogContent>
       <DialogActions>
